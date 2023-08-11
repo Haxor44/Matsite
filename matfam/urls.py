5b9d85959e6e6acbd8fc8c8c7b040e19cfb9c859
Haxor44 <50330948+Haxor44@users.noreply.github.com>
2023-08-11 22:52:05 +0300

    Update urls.py

--- matfam/urls.py
+++ matfam/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
-from django.contrib.staticfiles.url import staticfiles_urlpatterns
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     path("matfamApp/", include("matfamApp.urls")),
     path("admin/", admin.site.urls),
