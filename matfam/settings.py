e778376ea7a4804c2f7c7df24f0f30c83f0fe9e6
Haxor44 <50330948+Haxor44@users.noreply.github.com>
2023-08-21 12:59:57 +0300

    Update settings.py

--- matfam/settings.py
+++ matfam/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'matfamApp',
+    'matfamApp.apps.matfamAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
