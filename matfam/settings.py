397983781510abdce87ee67f88442118132e580e
Haxor44 <50330948+Haxor44@users.noreply.github.com>
2023-08-21 13:11:10 +0300

    Update settings.py

--- matfam/settings.py
+++ matfam/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'matfamApp.apps.matfamAppConfig',
+    'matfamApp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
